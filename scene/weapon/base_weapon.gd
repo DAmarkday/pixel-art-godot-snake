508a5791e8ca978e0013f62620c418551c692dfc
xiejinqiao <damarkday@163.com>
2025-04-09 19:54:14 +0800

    feat:+1.添加枪声

--- scene/weapon/base_weapon.gd
+++ scene/weapon/base_weapon.gd
@@ -1,22 +1,26 @@
 extends Node2D
 class_name BaseWeapon
 
-const _pre_bullet = preload("res://scene/bullet/BaseBullet.tscn")
+# 子弹
+@export var bullet = preload("res://scene/bullet/BaseBullet.tscn")
 
 @export var bullets_per_magazine = 30  # 每弹夹子弹数
 @export var max_magazine_counts = 5 # 最大弹夹数量
 @export var cur_total_bullets_counts = 150  # 当前子弹数量
 @export var weapon_reload_rof= 0.42 # 武器换弹速度,数值越大换弹速度越慢
 
-@export var damage = 5
-@export var weapon_rof = 0.2
-@export var weapon_name = '默认枪械'
+@export var damage = 5 # 武器伤害
+@export var weapon_rof = 0.2  # 武器射击间隔,数值越大 两次射击的间隔越大。
+@export var weapon_name = '默认枪械' # 武器显示的文本
+# 射击音频
+@export var shoot_audio = preload("res://audio/wpn_fire_m4.mp3")
 
 var player:Player
 
 
-@onready var sprite = $Sprite2D
-@onready var bullet_point = $BulletPoint
+@onready var Sprite = $Sprite2D
+@onready var BulletPoint = $BulletPoint
+@onready var ShootAudio = $ShootAudio
 
 var current_rof_tick = 0
 
@@ -24,26 +28,28 @@ var current_bullet_count_in_single_magazine = 0 # 在当前弹夹中所有的子
 var current_magazine_counts = 0 # 当前的弹夹数量
 
 func shoot():
-	var instance = _pre_bullet.instantiate()
-	instance.global_position = bullet_point.global_position
+	var instance = bullet.instantiate()
+	instance.global_position = BulletPoint.global_position
 	instance.dir = global_position.direction_to(get_global_mouse_position())
 	
 	
 	# 使用枪械的朝向计算子弹方向
-	var direction = Vector2(cos(sprite.global_rotation), sin(sprite.global_rotation)).normalized()
+	var direction = Vector2(cos(Sprite.global_rotation), sin(Sprite.global_rotation)).normalized()
 	instance.dir = direction
 	
 	# 可选：设置子弹旋转，使长方形朝向与移动方向一致
-	instance.rotation = sprite.global_rotation
+	instance.rotation = Sprite.global_rotation
 	
 	#instance.current_weapon = self 
 	get_tree().root.add_child(instance)
+	ShootAudio.play()
 	
 	#current_bullet_count_in_single_magazine -=1
 	#weapon_anim()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	ShootAudio.stream = shoot_audio
 	pass # Replace with function body.
 
 
